cd1037fcaa8399bf0f01ba39ca9dbdd7f7f70ada
AnhTtis <nguyenhuuanhtri866@gmail.com>
2024-11-06 20:27:46 +0700

    Adjust text

--- SVGDraw.cpp
+++ SVGDraw.cpp
@@ -14,6 +14,7 @@ SVGDraw::~SVGDraw()
 
 void SVGDraw::loadSVGFile(const std::string &filename)
 {
+    bool isHead = false;
     SVGParser parser(filename);
     xml_node<> *rootNode = parser.getRootNode();
 
@@ -23,7 +24,10 @@ void SVGDraw::loadSVGFile(const std::string &filename)
 
         if (shape == "text")
         {
-            figures.push_back(new MyFigure::Text(node, graphics));
+            if (!isHead)
+                isHead = true;
+            else
+                figures.push_back(new MyFigure::Text(node, graphics));
         }
         else if (shape == "rect")
         {
--- ShapeDraw/Text.cpp
+++ ShapeDraw/Text.cpp
@@ -25,22 +25,22 @@ void MyFigure::Text::printInfomation()
 void MyFigure::Text::draw()
 {
     // Draw the text
-  
-        // Get fill color and adjust opacity
-        Color fillColor = attributes.getFillColor();
-        int opacity = attributes.getFillOpacity() * 255;
-        fillColor = Color(opacity, fillColor.GetR(), fillColor.GetG(), fillColor.GetB());
-        SolidBrush brush(fillColor);
 
-        // Set font
-        FontFamily fontFamily(L"Times New Roman");
-        Font fontDraw(&fontFamily, this->font, FontStyleRegular, UnitPixel);
+    // Get fill color and adjust opacity
+    Color fillColor = attributes.getFillColor();
+    int opacity = attributes.getFillOpacity() * 255;
+    fillColor = Color(opacity, fillColor.GetR(), fillColor.GetG(), fillColor.GetB());
+    SolidBrush brush(fillColor);
 
-        // Convert text content to wide string
-        string text = attributes.getText();
-         std::wstring wideText(text.begin(), text.end());
-        // Draw text
-        PointF pointF(point.getX(), point.getY());
-        graphics.DrawString(wideText.c_str(), -1, &fontDraw, pointF, &brush);
+    // Set font
+    FontFamily fontFamily(L"Times New Roman");
+    Font fontDraw(&fontFamily, this->font, FontStyleRegular, UnitPixel);
+
+    // Convert text content to wide string
+    string text = attributes.getText();
+    std::wstring wideText(text.begin(), text.end());
     
+    // Adjust the Y coordinate to move the text up
+    PointF pointF(point.getX(), point.getY() * 14 / 15); // Adjust the value as needed
+    graphics.DrawString(wideText.c_str(), -1, &fontDraw, pointF, &brush);
 }
