dc8a90bae4a01cd483b2104c33e1f5fecb6c568e
AnhTtis <nguyenhuuanhtri866@gmail.com>
2024-12-11 23:35:05 +0700

    RUn

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ VOID OnPaint(HDC hdc)
 {
     Graphics graphics(hdc);
     FigureDraw FigureDraw(graphics);
-    FigureDraw.loadSVGFile("testSVG/svg-15.svg");
+    FigureDraw.loadSVGFile("testSVG/svg-09.svg");
     FigureDraw.draw();
 }
 
