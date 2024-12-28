30df49ce6e65556f527551535efe6aa54e0a6e97
TKin <letrungkienthd@gmail.com>
2024-12-28 13:49:02 +0700

    added: new samples

--- FigureDraw.cpp
+++ FigureDraw.cpp
@@ -21,43 +21,47 @@ void FigureDraw::loadSVGFile(const std::string &filename)
 
     for (xml_node<> *node = rootNode->first_node(); node; node = node->next_sibling())
     {
-        std::string shape = node->name();
+        std::string element = node->name();
 
-        if (shape == "text")
+        if (element == "text")
         {
             figures.push_back(new MyFigure::Text(node, graphics));
         }
-        else if (shape == "rect")
+        else if (element == "rect")
         {
             figures.push_back(new MyFigure::Rectangle(node, graphics));
         }
-        else if (shape == "line")
+        else if (element == "line")
         {
             figures.push_back(new MyFigure::Line(node, graphics));
         }
-        else if (shape == "polygon")
+        else if (element == "polygon")
         {
             figures.push_back(new MyFigure::Polygon(node, graphics));
         }
-        else if (shape == "polyline")
+        else if (element == "polyline")
         {
             figures.push_back(new MyFigure::Polyline(node, graphics));
         }
-        else if (shape == "ellipse")
+        else if (element == "ellipse")
         {
             figures.push_back(new MyFigure::Ellipse(node, graphics));
         }
-        else if (shape == "circle")
+        else if (element == "circle")
         {
             figures.push_back(new MyFigure::Circle(node, graphics));
         }
-        else if (shape == "path")
+        else if (element == "path")
         {
             figures.push_back(new MyFigure::Path(node, graphics));
         }
-        else if (shape == "g")
+        else if (element == "g")
         {
             figures.push_back(new MyFigure::Group(node, graphics));
+        }
+        else if (element == "def")
+        {
+            // implement here
         }
     }
 }
